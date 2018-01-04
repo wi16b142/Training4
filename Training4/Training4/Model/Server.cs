3513051f743f65443319e02829a4d111d4cf9c5b
Sascha Böck <wi16b142@technikum-wien.at>
2018-01-04 23:03:09 +0100

    added todos to classes and VMs

---
+++ Training4/Training4/Model/Server.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training4.Model
+{
+    public class Server
+    {
+        //serversock
+        //list ch
+        //acceptingthread
+        //broadcast
+        //guiupdate
+    }
+}
---
+++ Training4/Training4/ViewModel/ProductVM.cs
@@ -0,0 +1,14 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training4.ViewModel
+{
+    public class ProductVM:ViewModelBase
+    {
+        //props
+    }
+}
